1c38efa73509d0db5713e9ae58bd5b3b9232a4e5
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-20 17:46:24 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,15 +1,6 @@
 class Hash
   def keys_of(*arguments)
     # code goes here
-    output = []
-    
-    each do |key, value|
-      arguments.each do |arg|  
-        if arg == key
-          output << arg
-        end
-      end
-    end  
-    output
+     map {|key, value| args.include?(value) ? key : nil }.compact
   end
 end
